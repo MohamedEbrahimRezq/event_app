b9edd9894c4c517a61f086b8e2a748b2ce2b857a
MohamedEbrahimRezq <164947638+MohamedEbrahimRezq@users.noreply.github.com>
2024-12-19 01:25:36 +0200

    set the localizations

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -1,17 +1,110 @@
+import 'package:event_planning_app/home_screen/language_sheet.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const String routeName = 'home_screen';
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
-        title: Text(AppLocalizations.of(context)!.language),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(AppLocalizations.of(context)!.language, style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+              color: Colors.black
+            ),),
+            SizedBox(height: height*0.03),
+            InkWell(
+              onTap: (){
+                showLanguageBottomSheet();
+              },
+              child: Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  border: Border.all(
+                    color: Colors.blue,
+                    width: 2
+                  )
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(AppLocalizations.of(context)!.english,
+                      style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.black
+                    )
+                    ),
+                    Icon(Icons.arrow_drop_down)
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: height*0.03),
+            Text(AppLocalizations.of(context)!.theme, style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+                color: Colors.black
+            ),),
+            SizedBox(height: height*0.03),
+            InkWell(
+              onTap: (){
+
+              },
+              child: Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(
+                        color: Colors.blue,
+                        width: 2
+                    )
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(AppLocalizations.of(context)!.theme,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.black
+                        )
+                    ),
+                    Icon(Icons.arrow_drop_down)
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+
+  void showLanguageBottomSheet (){
+    showModalBottomSheet(
+        context: context,
+        builder: (context) => LanguageBottomSheet()
+    );
+  }
 }
+
+
+
