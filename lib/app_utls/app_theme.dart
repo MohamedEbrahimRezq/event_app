aa5b14f4ff4025fd73e491d95d0ba907b424c514
Mustafa Musa <Msm.musaa@gmail.com>
2024-12-21 16:30:23 +0200

    BottomNavigationBar UI

--- lib/app_utls/app_theme.dart
+++ lib/app_utls/app_theme.dart
@@ -3,15 +3,46 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static final ThemeData lightTheme = ThemeData(
-    appBarTheme: AppBarTheme(
-      backgroundColor: AppColors.light,
-    )
+    textTheme:  TextTheme(bodyMedium: TextStyle(
+      color: AppColors.black
+    )),
+    scaffoldBackgroundColor: AppColors.bglight,
+    primaryColor: AppColors.primaryColorLight,
+      floatingActionButtonTheme:const FloatingActionButtonThemeData(
+        backgroundColor: AppColors.primaryColorLight,
+        shape: StadiumBorder(
+          side: BorderSide(
+            color: AppColors.white,
+            width: 5,
+
+          ),
+        ),
+      ),
+      bottomNavigationBarTheme:const BottomNavigationBarThemeData(
+        elevation: 0,
+        showUnselectedLabels: true,
+      ),
   );
   static final ThemeData darkTheme = ThemeData(
-      appBarTheme: AppBarTheme(
-        backgroundColor: AppColors.dark,
-      )
+      textTheme:  TextTheme(bodyMedium: TextStyle(
+          color: AppColors.white
+      )),
+      scaffoldBackgroundColor: AppColors.primaryColorDark,
+      primaryColor: AppColors.primaryColorDark,
+      floatingActionButtonTheme:const FloatingActionButtonThemeData(
+        backgroundColor: AppColors.primaryColorLight,
+        shape: StadiumBorder(
+          side: BorderSide(
+            color: AppColors.white,
+            width: 5,
 
+          ),
+        ),
+      ),
+  bottomNavigationBarTheme:const BottomNavigationBarThemeData(
+    elevation: 0,
+    showUnselectedLabels: true,
+  )
 
   );
 
---
+++ lib/app_utls/assets_manager.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+class AssetsManager {
+  static const String iconHome = "assets/images/iconHome.png";
+  static const String iconMap = "assets/images/iconMap.png";
+  static const String iconLike = "assets/images/unslected_like_tab.png";
+  static const String iconProfile = "assets/images/unslected_pofile_tab.png";
+  static const String iconHomeSelected = "assets/images/selected_home_tab.png";
+  static const String iconMapSelected = "assets/images/selected_map_tab.png";
+  static const String iconLikeSelected = "assets/images/selected_like_tab.png";
+  static const String iconProfileSelected = "assets/images/selected_profile_tab.png";
+}
