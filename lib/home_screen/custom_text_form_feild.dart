e870c45bea4015010a17cb78b6cd2434d58ba15a
Mustafa Musa <Msm.musaa@gmail.com>
2024-12-28 12:33:11 +0200

    Like Tab UI

--- lib/home_screen/custom_text_form_feild.dart
+++ lib/home_screen/custom_text_form_feild.dart
@@ -1,12 +1,51 @@
+import 'package:event_planning_app/app_utls/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import '../app_utls/app_styles.dart';
 
 class CustomTextFormFeild extends StatelessWidget {
-  const CustomTextFormFeild({super.key});
+  Color borderColor;
+  IconData? preIcon;
+  IconData? afterIcon;
+  String? hintText;
+  int? numLines;
+  CustomTextFormFeild({
+    required this.borderColor,
+    this.preIcon,
+    this.afterIcon,
+    this.hintText,
+    this.numLines
+  });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-
+      onTap: (){},
+      maxLines: numLines,
+      style: AppStyle.primary14bold.copyWith(
+        color: borderColor
+      ),
+      cursorColor: borderColor,
+      decoration: InputDecoration(
+        enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color:borderColor ,width: 1,),
+            borderRadius: BorderRadius.circular(16)
+        ),
+        errorBorder: OutlineInputBorder(
+            borderSide: BorderSide(color:AppColors.red ,width: 1,),
+            borderRadius: BorderRadius.circular(16)
+        ),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color:borderColor ,width: 1,),
+          borderRadius: BorderRadius.circular(16)
+        ),
+        prefixIcon: Icon( preIcon ,color: borderColor,),
+        suffixIcon: Icon( afterIcon ,color: borderColor,),
+        hintStyle:  AppStyle.primary14bold.copyWith(
+            color: borderColor
+        ),
+        hintText:hintText,
+      ),
     );
   }
 }
--- lib/tabs/likeTab/like_tab.dart
+++ lib/tabs/likeTab/like_tab.dart
@@ -2,6 +2,7 @@ import 'package:event_planning_app/home_screen/custom_text_form_feild.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../app_utls/app_colors.dart';
 import '../../app_utls/assets_manager.dart';
 import '../../provider/language_provider.dart';
 import '../../provider/theme_provider.dart';
@@ -18,35 +19,67 @@ class LikeTab extends StatelessWidget {
     var languageProvider = Provider.of<AppLanguageProvider>(context);
     var themeProvider = Provider.of<AppThemeProvider>(context);
     return Scaffold(
-      body: Column(
-        children: [
-
-          SizedBox(
-            height: height*.07,
-          ),
-          CustomTextFormFeild(),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: height * .07,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: CustomTextFormFeild(
+                borderColor: AppColors.primaryColorLight,
+                preIcon: Icons.search_outlined,
+                hintText: AppLocalizations.of(context)!.search,
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
                 child: Column(
                   children: [
-                    EventWidget(imageBg: AssetsManager.birthdayBg,eventName: AppLocalizations.of(context)!.birthday,),
-                    EventWidget(imageBg: AssetsManager.sportBg,eventName: AppLocalizations.of(context)!.sport,),
-                    EventWidget(imageBg: AssetsManager.meetingBg,eventName: AppLocalizations.of(context)!.meeting,),
-                    EventWidget(imageBg: AssetsManager.gamingBg,eventName: AppLocalizations.of(context)!.gaming,),
-                    EventWidget(imageBg: AssetsManager.eatingBg,eventName: AppLocalizations.of(context)!.eating,),
-                    EventWidget(imageBg: AssetsManager.holidayBg,eventName: AppLocalizations.of(context)!.holiday,),
-                    EventWidget(imageBg: AssetsManager.exhibitionBg,eventName: AppLocalizations.of(context)!.exhibition,),
-                    EventWidget(imageBg: AssetsManager.bookclubBg,eventName: AppLocalizations.of(context)!.book_club,),
-                    EventWidget(imageBg: AssetsManager.workshopBg,eventName: AppLocalizations.of(context)!.work_shop,),
-
+                    EventWidget(
+                      imageBg: AssetsManager.birthdayBg,
+                      eventName: AppLocalizations.of(context)!.birthday,
+                    ),
+                    EventWidget(
+                      imageBg: AssetsManager.sportBg,
+                      eventName: AppLocalizations.of(context)!.sport,
+                    ),
+                    EventWidget(
+                      imageBg: AssetsManager.meetingBg,
+                      eventName: AppLocalizations.of(context)!.meeting,
+                    ),
+                    EventWidget(
+                      imageBg: AssetsManager.gamingBg,
+                      eventName: AppLocalizations.of(context)!.gaming,
+                    ),
+                    EventWidget(
+                      imageBg: AssetsManager.eatingBg,
+                      eventName: AppLocalizations.of(context)!.eating,
+                    ),
+                    EventWidget(
+                      imageBg: AssetsManager.holidayBg,
+                      eventName: AppLocalizations.of(context)!.holiday,
+                    ),
+                    EventWidget(
+                      imageBg: AssetsManager.exhibitionBg,
+                      eventName: AppLocalizations.of(context)!.exhibition,
+                    ),
+                    EventWidget(
+                      imageBg: AssetsManager.bookclubBg,
+                      eventName: AppLocalizations.of(context)!.book_club,
+                    ),
+                    EventWidget(
+                      imageBg: AssetsManager.workshopBg,
+                      eventName: AppLocalizations.of(context)!.work_shop,
+                    ),
                   ],
                 ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
