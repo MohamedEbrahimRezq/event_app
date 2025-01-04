7f6412ebb35fccfdefd7e9152c6b7a58581a6976
Mustafa Musa <Msm.musaa@gmail.com>
2025-01-05 00:35:17 +0200

    favorite icon

--- lib/provider/event_list_provider.dart
+++ lib/provider/event_list_provider.dart
@@ -2,23 +2,30 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../fire_base/firebase_files.dart';
 import '../fire_base/model/events.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class EventListProvider extends ChangeNotifier {
   int selectedTab = 0;
   List<Event> addedEventList = [];
   List<Event> filteredEventList = [];
-  List<String> eventList = [
-    'All',
-    'Sport',
-    'Birthday',
-    'Meeting',
-    'Gaming',
-    'Eating',
-    'Holiday',
-    'Exhibition',
-    'Book Club',
-    'Work Shop',
-  ];
+  List<String> eventList = [];
+
+  void getEventNameList(BuildContext context){
+
+    eventList = [
+      AppLocalizations.of(context)!.all,
+      AppLocalizations.of(context)!.sport,
+      AppLocalizations.of(context)!.birthday,
+      AppLocalizations.of(context)!.meeting,
+      AppLocalizations.of(context)!.gaming,
+      AppLocalizations.of(context)!.eating,
+      AppLocalizations.of(context)!.holiday,
+      AppLocalizations.of(context)!.exhibition,
+      AppLocalizations.of(context)!.book_club,
+      AppLocalizations.of(context)!.work_shop,
+    ];
+  }
+
 
   void getAllEvents() async {
     QuerySnapshot<Event> querySnapshot =
@@ -31,6 +38,7 @@ class EventListProvider extends ChangeNotifier {
   }
 
   void getFilteredEvents() async {
+    getEventNameList;
     QuerySnapshot<Event> querySnapshot =
         await FirebaseFiles.getEventCollection().get();
     addedEventList = querySnapshot.docs.map((doc) {
