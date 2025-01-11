c6024aa3282d9d72c997706555317bb61f96dd92
MohamedEbrahimRezq <164947638+MohamedEbrahimRezq@users.noreply.github.com>
2025-01-11 14:28:00 +0200

    Sign in with Google

--- lib/fire_base/firebase_files.dart
+++ lib/fire_base/firebase_files.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:event_planning_app/fire_base/model/myUser.dart';
 import 'model/events.dart';
 
 class FirebaseFiles {
@@ -9,6 +10,23 @@ class FirebaseFiles {
             fromFirestore: (snapshot, option) =>
                 Event.fromFireStore(snapshot.data()!),
             toFirestore: (event, _) => event.toFireStore());
+  }
+
+  static CollectionReference<MyUser> getUsersCollection (){
+    return FirebaseFirestore.instance.collection(MyUser.collectionName)
+        .withConverter(
+        fromFirestore: (snapshot, options) => MyUser.fromFireStore(snapshot.data()),
+        toFirestore: (myUser,_) => myUser.toFireStore()
+    );
+  }
+
+  static Future<void> addUserToFireStore (MyUser myUser) {
+    return getUsersCollection().doc(myUser.id).set(myUser);
+  }
+
+  static Future<MyUser?> readUserFromFireStore (String id) async{
+    var querySnapshot = await getUsersCollection().doc(id).get();
+    return querySnapshot.data();
   }
 
   static Future<void> addEventToFireStore(Event event) {
